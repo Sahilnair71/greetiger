550c857c6d6a09f7b35842975f1ecef636e189f3
belkin <sahilnair71@gmail.com>
2021-04-22 03:48:44 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { MatToolbarModule } from "@angular/material/toolbar";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from "./navbar/navbar.component";
-import { MatIconModule } from "@angular/material/icon";
-import { MatSelectModule } from "@angular/material/select";
-import { MatFormFieldModule } from "@angular/material/form-field";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,15 +15,7 @@ import { MatFormFieldModule } from "@angular/material/form-field";
     FooterComponent,
     NavbarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatSelectModule,
-    MatFormFieldModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
